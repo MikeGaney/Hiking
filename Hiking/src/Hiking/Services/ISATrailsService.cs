8fb3034fd31671257a33c83f3e3e1a7eab7b8ca6
Sarah Andrea <comet9050@aol.com>
2016-04-19 15:41:52 -0700

    edit/add

--- Hiking/src/Hiking/Services/ISATrailsService.cs
+++ Hiking/src/Hiking/Services/ISATrailsService.cs
@@ -6,5 +6,9 @@ namespace Hiking.Services
     public interface ISATrailsService
     {
         List<Trail> GetTrailsList();
+        void AddTrail(Trail trail);
+
+        Trail GetOneTrail(int id);
+        void EditTrail(Trail trail);
     }
 }
